5798954076c0c6713ef35a149b65293a8586c71c
André Pires <andrebeckpires@gmail.com>
2022-05-10 20:15:40 -0300

    enhancments

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-var xs = [];
-var os = [];
+var marks = [];
+
 var squares = [];
 var player = 0;
 for (var i = 0; i < 9; i++) {
-  xs[i] = document.getElementsByClassName("x")[i];
-}
-for (var i = 0; i < 9; i++) {
-  os[i] = document.getElementsByClassName("o")[i];
+  marks[i] = document.getElementsByClassName("mark")[i];
 }
 
 for (var i = 0; i < 9; i++) {
@@ -15,90 +12,120 @@ for (var i = 0; i < 9; i++) {
 
 squares[0].addEventListener("click", () => {
   if (player % 2 == 0) {
-    xs[0].style.visibility = "visible";
+    marks[0].innerHTML = "x";
+    marks[0].style.color = "red";
   } else {
-    os[0].style.visibility = "visible";
+    marks[0].innerHTML = "o";
+    marks[0].style.color = "blue";
   }
+  marks[0].style.visibility = "visible";
   player++;
 });
 
 squares[1].addEventListener("click", () => {
   if (player % 2 == 0) {
-    xs[1].style.visibility = "visible";
+    marks[1].innerHTML = "x";
+    marks[1].style.color = "red";
   } else {
-    os[1].style.visibility = "visible";
+    marks[1].innerHTML = "o";
+    marks[1].style.color = "blue";
   }
+  marks[1].style.visibility = "visible";
   player++;
 });
 
 squares[2].addEventListener("click", () => {
   if (player % 2 == 0) {
-    xs[2].style.visibility = "visible";
+    marks[2].innerHTML = "x";
+    marks[2].style.color = "red";
   } else {
-    os[2].style.visibility = "visible";
+    marks[2].innerHTML = "o";
+    marks[2].style.color = "blue";
   }
+  marks[2].style.visibility = "visible";
   player++;
 });
 
 squares[3].addEventListener("click", () => {
   if (player % 2 == 0) {
-    xs[3].style.visibility = "visible";
+    marks[3].innerHTML = "x";
+    marks[3].style.color = "red";
   } else {
-    os[3].style.visibility = "visible";
+    marks[3].innerHTML = "o";
+    marks[3].style.color = "blue";
   }
+  marks[3].style.visibility = "visible";
   player++;
 });
 
 squares[4].addEventListener("click", () => {
   if (player % 2 == 0) {
-    xs[4].style.visibility = "visible";
+    marks[4].innerHTML = "x";
+    marks[4].style.color = "red";
   } else {
-    os[4].style.visibility = "visible";
+    marks[4].innerHTML = "o";
+    marks[4].style.color = "blue";
   }
+  marks[4].style.visibility = "visible";
   player++;
 });
 
 squares[5].addEventListener("click", () => {
   if (player % 2 == 0) {
-    xs[5].style.visibility = "visible";
+    marks[5].innerHTML = "x";
+    marks[5].style.color = "red";
   } else {
-    os[5].style.visibility = "visible";
+    marks[5].innerHTML = "o";
+    marks[5].style.color = "blue";
   }
+  marks[5].style.visibility = "visible";
   player++;
 });
 
 squares[6].addEventListener("click", () => {
   if (player % 2 == 0) {
-    xs[6].style.visibility = "visible";
+    marks[6].innerHTML = "x";
+    marks[6].style.color = "red";
   } else {
-    os[6].style.visibility = "visible";
+    marks[6].innerHTML = "o";
+    marks[6].style.color = "blue";
   }
+  marks[6].style.visibility = "visible";
   player++;
 });
 
 squares[7].addEventListener("click", () => {
   if (player % 2 == 0) {
-    xs[7].style.visibility = "visible";
+    marks[7].innerHTML = "x";
+    marks[0].style.color = "red";
   } else {
-    os[7].style.visibility = "visible";
+    marks[7].innerHTML = "o";
+    marks[0].style.color = "blue";
   }
+  marks[7].style.visibility = "visible";
   player++;
 });
 
 squares[8].addEventListener("click", () => {
   if (player % 2 == 0) {
-    xs[8].style.visibility = "visible";
+    marks[8].innerHTML = "x";
+    marks[0].style.color = "red";
   } else {
-    os[8].style.visibility = "visible";
+    marks[8].innerHTML = "o";
+    marks[0].style.color = "blue";
   }
+  marks[8].style.visibility = "visible";
   player++;
 });
 
 squares[9].addEventListener("click", () => {
   if (player % 2 == 0) {
-    xs[9].style.visibility = "visible";
+    marks[9].innerHTML = "x";
+    marks[0].style.color = "red";
   } else {
-    os[9].style.visibility = "visible";
+    marks[9].innerHTML = "o";
+    marks[0].style.color = "blue";
   }
+  marks[9].style.visibility = "visible";
   player++;
 });
